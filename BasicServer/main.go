a255316394cef7ea9e1ba5e378dfab24cb0e2232
lakhan kumar <lakhanrathi92@gmail.com>
2021-06-03 15:12:14 +0200

    hello world via http server

--- BasicServer/main.go
+++ BasicServer/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	h1 := func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
-	})
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Something went wrong", http.StatusBadGateway)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	}
 
-	h1 := func(w http.ResponseWriter, _ *http.Request) {
+	h2 := func(w http.ResponseWriter, _ *http.Request) {
 		log.Println("Good bye World")
 	}
 
-	http.HandleFunc("/goodbye", h1)
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/goodbye", h2)
 
 	http.ListenAndServe("127.0.0.1:9090", nil)
 }
