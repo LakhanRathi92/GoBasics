cde39b151bcc5efddd880d431d90fac21b2f599c
lakhan kumar <lakhanrathi92@gmail.com>
2021-06-03 21:41:11 +0200

    restructuring

--- BasicServer/handlers/helloHandler.go
+++ BasicServer/handlers/helloHandler.go
@@ -1,20 +1,22 @@
 package handlers
 
 import (
-	"log"	"net/http"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
-
 
 type HelloHandler struct {
 	l *log.Logger
 }
 
-func newHelloHandler(l *log.Logger) {
-	&HelloHandler{l}
+func NewHelloHandler(l *log.Logger) *HelloHandler {
+	return &HelloHandler{l}
 }
 
-func (h* HelloHandler) serveHttp(rw http.ResponseWriter, r *http.Request){
-	h.l.Println("Hello World");
+func (h *HelloHandler) ServeHttp(rw http.ResponseWriter, r *http.Request) {
+	h.l.Println("Hello World")
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, "Something went wrong", http.StatusBadGateway)
--- BasicServer/main.go
+++ BasicServer/main.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+func main() {
+	http.ListenAndServe("127.0.0.1:9090", nil)
 }
