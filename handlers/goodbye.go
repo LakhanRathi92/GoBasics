29f6dfe6d77a3bdec236ffff100533c96198014d
lakhan kumar <lakhanrathi92@gmail.com>
2021-06-05 01:38:37 +0200

    bye handler

---
+++ handlers/goodbye.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodBye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (h *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Ooops", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw, "Good bye %s", d)
+}
--- main.go
+++ main.go
@@ -12,9 +12,12 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handlers.NewHello(l)
+	helloHandler := handlers.NewHello(l)
+	byeHandler := handlers.NewGoodBye(l)
 	sm := http.NewServeMux()
 
-	sm.Handle("/", hh)
+	sm.Handle("/hello", helloHandler)
+	sm.Handle("/bye", byeHandler)
+
 	http.ListenAndServe("127.0.0.1:9090", sm)
 }
