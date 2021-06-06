5c0179475f874daf86d80c5ca1712199e8b04638
lakhan kumar <lakhanrathi92@gmail.com>
2021-06-06 15:20:39 +0200

    go routine to handle server graceful shutdown

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 //Building Microservices with Go on youtube by Nic Jackson
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/LakhanRathi92/GoBasics/handlers"
@@ -28,5 +30,23 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	//register OS signal
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Recieved terminate signal, graceful shutdown.... ", sig)
+
+	//graceful shutdown. Wait for 30 seconds on existing handlers.
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
